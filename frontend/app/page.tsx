9b15a43b9106f76c9d5ce1b00137abedbe4b8cec
Teddaudi <daudited@gmail.com>
2024-09-30 12:26:40 +0300

    starter

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,39 +1,6 @@
-"use client"
-import { useEffect, useState } from "react";
-import SideNavbar from "./components/SideNavbar";
 
 export default function Home() {
-  const [data, setData] = useState(null); 
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("/api", {
-          headers: {
-            Accept: "application/json",
-          },
-          method: "GET",
-        });
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-
-        const jsonData = await response.json();
-        setData(jsonData); 
-      } catch (err) {
-        throw err;
-      }
-    };
-
-    fetchData();
-  }, []); 
-
-
   return (
-    <div>
-      <SideNavbar/>
-    </div>
+    <div>Home</div>
   );
 }
